78699ce01e170df9a571995909d53d30d9ecd675
dhumphrey96 <danthumphrey@gmail.com>
2017-02-16 19:00:09 -0500

    Update prog03.cpp

--- prog03.cpp
+++ prog03.cpp
@@ -1,4 +1,4 @@
-// Name:         Daniel Humphrey & NIC KNOCH
+// Name:         Daniel Humphrey
 // Program:      CSC 275 Program 3
 // Due Date:     02/19/17
 // Compiler:     GNU GCC
@@ -27,18 +27,18 @@ int main() {
     cout<<"Input the size of the database: "<<endl;
     cin>>sizet;
     cout<<"Size is: " <<sizet<<endl;
-    Student* array;
-    array = new Student[sizet];
+    Student* arry;
+    arry = new Student[sizet];
 
     int i = 0;
             //loop for inputing the data
     while (!(i == sizet) ) {                    //i is counter of number of people
-        cin >> array[i];            //input the data
+        cin >> arry[i];            //input the data
         i++;
     }
-    cout<<"You have entered the database maintenance mode. Type HELP for a list of commands, thank you!"<<endl;
-    cout<<"Note: is case sensitive."<<endl;
+
     while (isRunning ==true){
+        cout<<"You have entered the database maintenance mode. \nType help for a list of commands, thank you!"<<endl;
         cin >> choice;
         if(choice == "quit"){
             cout<<"You have selected the quit function, thank you!"<<endl;
@@ -47,15 +47,19 @@ int main() {
         }
         else if(choice == "add"){
             cout<<"You have selected the add function, thank you!"<<endl;
+            addStudent(arry,sizet);
         }
         else if(choice == "list"){
             cout<<"You have selected the list function, thank you!"<<endl;
             for(int j=0; j<sizet; j++){
-                cout<<array[j]<<endl;
+                cout<<arry[j]<<endl;
             }
         }
         else if(choice == "delclass"){
+            int index, indexofClass;
             cout<<"You have selected the delclass function, thank you!"<<endl;
+            cin>>index>>indexofClass;
+            arry[index].delClass(indexofClass);
         }
         else if(choice == "delete"){
             cout<<"You have selected the delete function, thank you!"<<endl;
@@ -64,7 +68,18 @@ int main() {
             cout<<"You have selected the reset function, thank you!"<<endl;
         }
         else if(choice == "newclass"){
+            int index;
             cout<<"You have selected the newclass function, thank you!"<<endl;
+            cin>>index;
+            arry[index].newClass();
+        }
+        else if(choice == "help"){
+                cout<<"reset - Resets to default"<<endl;
+                cout<<"reset - Resets to default"<<endl;
+
+        }
+        else{
+            cout<<"That is not a recognized command"<<endl;
         }
     }
 
