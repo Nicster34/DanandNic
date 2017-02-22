6345ddaa1b084f7431d21ff9e1a74a0a43c6875f
dhumphrey96 <danthumphrey@gmail.com>
2017-02-21 19:35:06 -0500

    final version of header file for prog 3

--- student.h
+++ student.h
@@ -1,29 +1,25 @@
-// Name:         Daniel Humphrey
+// Name:         Daniel Humphrey, Nicholas Knoch
 // Program:      CSC 275 Program 3
 // Due Date:     02/19/17
 // Compiler:     GNU GCC
 
+//Interface
 
-//interface
 class Student{
-
     public:
-        friend istream& operator >>(istream&,Student&);
-        friend ostream& operator <<(ostream& out, const Student& rhs);
-        Student();
-        Student(string, string, int);
-        void newClass();
-        void delClass(int);
-
-
-
-
+        friend istream& operator >>(istream&,Student&);                         //Overloaded input operator
+        friend ostream& operator <<(ostream& out, const Student& rhs);          //Overloaded output operator
+        Student();                                                              //Default constructor
+        Student(string, string, int);                                           //Constructor
+        void newClass();                                                        //Function to create a new class for a particular student
+        void delClass(int);                                                     //Function to delete a class from a particular student
+        void reset();                                                           //Function to reset a particular student's data to empty
+        void operator =(const Student&);                                        //Overloaded assignment operator
+        ~Student();                                                             //Destructor
     private:
-        string lname;
-        string fname;
-        int nclasses;
-        string* classList;
-
-
+        string lname;                                                           //Last name of student
+        string fname;                                                           //First name of student
+        int nclasses;                                                           //Number of classes for a student
+        string* classList;                                                      //List of all the classes for a student
 
 };
